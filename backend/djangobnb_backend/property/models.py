a917ea66b7d5a3fb789075f2f7dc643d97b889a7
YatiZ <zuzu62113@gmail.com>
2024-11-02 10:01:15 +0630

    testing-reservation

--- backend/djangobnb_backend/property/models.py
+++ backend/djangobnb_backend/property/models.py
@@ -16,6 +16,7 @@ class Property(models.Model):
     country_code = models.CharField(max_length=10)
     category = models.CharField(max_length=255)
     image = models.ImageField(upload_to='uploads/properties')
+    favorited = models.ManyToManyField(User, related_name='favorites', blank=True)
     landlord = models.ForeignKey(User, related_name='properties',on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
